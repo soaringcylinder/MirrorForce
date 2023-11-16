10395594f5141a5177e1b49012e9101042f69c92
Future Wayne <futurewayne.ludens@gmail.com>
2023-11-16 12:25:43 -0700

    Modify bullet base, adding attributes clamping

--- Source/MirrorForce/Private/AbilitySystem/MirrorAttributeSet.cpp
+++ Source/MirrorForce/Private/AbilitySystem/MirrorAttributeSet.cpp
@@ -8,4 +8,18 @@ UMirrorAttributeSet::UMirrorAttributeSet()
 	InitMaxHealth(3.0f);
 }
 
+void UMirrorAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp<float>(NewValue, 0.f, GetMaxHealth());
+	}
+	else if (Attribute == GetMaxHealthAttribute())
+	{
+		NewValue = FMath::Max<float>(NewValue, 0.f);
+	}
+}
+
 
